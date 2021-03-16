a2064989ccee3a14b34c0cc354904e6b88821393
Sonny <sonny.lepapa@gmail.com>
2021-03-16 11:42:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+	if (_.has(req.session, 'user') === false)
+		req.session['user'] = { logged: false };
+	next();
+});
+
+app.disable('x-powered-by');
+
 app.use('/dist', express.static(`${__dirname}/dist`));
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
 
-const port = process.env.PORT || 3000;
-app.listen(port, process.env.IP || '0.0.0.0', () => console.log('Server started as :', port));
+const server = app.listen(3000, () => {
+  const host = server.address().address, port = server.address().port;
+	console.log(`[HTTP] Server listening on ${host}:${port}`);
+});

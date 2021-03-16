4ea22e028ebd5f76ae99277f912b8596f5cf457c
Sonny <sonny.lepapa@gmail.com>
2021-03-16 11:30:02 +0100

    Update App.js to function component

--- assets/App.js
+++ assets/App.js
@@ -3,19 +3,8 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-
-    componentDidMount() {
-        // Function called when component mount
-    }
-
-    render() {
-        return (<div className='app'>Base react project</div>)
-    }
+function App({ }) {
+    return <div className='app'>Hello<div/>;
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));

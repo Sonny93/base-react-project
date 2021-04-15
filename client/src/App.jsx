fac3c1980f25072f71e7b9fab9b3601ee5d03122
Sonny <sonny.lepapa@gmail.com>
2021-04-15 20:23:40 +0200

    Update default css file import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import '@assets/css/index.css';
+import '@assets/index.css';
 
 function App({ }) {
     return <div className='app'>hello</div>;
